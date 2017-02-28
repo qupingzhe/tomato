cc9413234d37e35eeecf2d414de4e8a9ffd8fd31
work <work@death>
2017-02-28 20:27:03 +0800

    should revise TomatoMainWindow::doStart for windows

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -41,7 +41,7 @@ void TomatoMainWindow::doStart( void )
 	clock_gettime(CLOCK_REALTIME, &time);
 	tm nowTime;
 	localtime_r(&time.tv_sec, &nowTime);
-	FILE* file = fopen("/home/work/study/git/tomato/date/date","a");
+	FILE* file = fopen("./date/date","a");
 	fprintf(file, "%04d %02d %02d ", nowTime.tm_year + 1900, nowTime.tm_mon, nowTime.tm_mday );
 	fprintf(file,"%02d:%02d:%02d\n", nowTime.tm_hour, nowTime.tm_min, nowTime.tm_sec);
 	fclose(file);
